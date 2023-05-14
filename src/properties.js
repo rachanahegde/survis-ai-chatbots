af30c09ae5afb0909fc5c54baaf9a637a938774d
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-05-14 16:31:06 +0100

    Add files via upload

--- src/properties.js
+++ src/properties.js
@@ -54,8 +54,7 @@ var editable = true;
  * @type {{html: string, id: string}}
  */
 var paper = {
-    html: 'Sample literature collection for <b><a href="http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/eurovis14-star.pdf" target="_blank">SurVis</a></b>',
-    id: 'Beck2016Visual'
+    html: 'A Survey of AI Chatbots with Behaviour Change Applications',
 };
 
 /**
